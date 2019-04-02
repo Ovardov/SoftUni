8ed89aa953e8efe25c1ff85356f8dc70a0c79180
Ovardov <ovardov7@gmail.com>
2019-04-02 20:41:47 +0300

    Add all solutions for Front-End Lab

---
+++ JavaScript-Applications/JavaScript-for-Front-End/Lab/JS-Core-Module/scripts/solution.js
@@ -0,0 +1,28 @@
+function attachEvents() {
+    let confirmationButton = $('#confirmation');
+    let yesButton = $('#confirm-collapse');
+
+
+    confirmationButton.on('show.bs.modal', showCollapse);
+    yesButton.click(applyCourse);
+
+    function showCollapse(event) {
+        let button = $(event.relatedTarget);
+
+        let course = button.data('course');
+
+        let modal = $(this);
+
+        modal.find('.modal-title').text(`Apply for the ${course} course`);
+        modal.find('.modal-body').text(`Are you sure you want to apply for the ${course} course?`);
+    }
+
+
+    function applyCourse() {
+        window.location.href='#'
+    }
+}
+
+
+
+
